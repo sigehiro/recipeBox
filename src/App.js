8b1d928c549f6c4044f3d064e0585fb48a56767f
BolosBolosGitHub416 <bolosbolosmpjcss@gmail.com>
2024-07-24 18:05:39 -0400

    first1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/cda</code> and save to reload.
+                    Edit <code>src/cda</code> and save to reload.test
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
